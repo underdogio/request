bb1e51aacbd458806c9cd4231c4727d4a978f349
Todd Wolfson <todd@twolfson.com>
2015-01-07 17:54:32 -0600

    Commenting out unused tests

--- tests/test-proxy.js
+++ tests/test-proxy.js
@@ -108,132 +108,132 @@ if (process.env.TEST_PROXY_HARNESS) {
 } else {
   // Run the real tests
 
-  runTest('basic proxy', {
-    proxy   : s.url,
-    headers : {
-      'proxy-authorization': 'Token Fooblez'
-    }
-  }, function(t, req, res) {
-    t.equal(req.headers['proxy-authorization'], 'Token Fooblez')
-  })
+  // runTest('basic proxy', {
+  //   proxy   : s.url,
+  //   headers : {
+  //     'proxy-authorization': 'Token Fooblez'
+  //   }
+  // }, function(t, req, res) {
+  //   t.equal(req.headers['proxy-authorization'], 'Token Fooblez')
+  // })
 
-  runTest('proxy auth without uri auth', {
-    proxy : 'http://user:pass@localhost:' + s.port
-  }, function(t, req, res) {
-    t.equal(req.headers['proxy-authorization'], 'Basic dXNlcjpwYXNz')
-  })
+  // runTest('proxy auth without uri auth', {
+  //   proxy : 'http://user:pass@localhost:' + s.port
+  // }, function(t, req, res) {
+  //   t.equal(req.headers['proxy-authorization'], 'Basic dXNlcjpwYXNz')
+  // })
 
-  runTest('HTTP_PROXY environment variable', {
-    env : { HTTP_PROXY : s.url }
-  }, true)
+  // runTest('HTTP_PROXY environment variable', {
+  //   env : { HTTP_PROXY : s.url }
+  // }, true)
 
-  runTest('http_proxy environment variable', {
-    env : { http_proxy : s.url }
-  }, true)
+  // runTest('http_proxy environment variable', {
+  //   env : { http_proxy : s.url }
+  // }, true)
 
-  runTest('http_proxy with length of one more than the URL', {
-    env: {
-      HTTP_PROXY : s.url,
-      NO_PROXY: 'elgoog1.com' // one more char than google.com
-    }
-  }, true)
+  // runTest('http_proxy with length of one more than the URL', {
+  //   env: {
+  //     HTTP_PROXY : s.url,
+  //     NO_PROXY: 'elgoog1.com' // one more char than google.com
+  //   }
+  // }, true)
 
-  runTest('NO_PROXY hostnames are case insensitive', {
-    env : {
-      HTTP_PROXY : s.url,
-      NO_PROXY   : 'GOOGLE.COM'
-    }
-  }, false)
+  // runTest('NO_PROXY hostnames are case insensitive', {
+  //   env : {
+  //     HTTP_PROXY : s.url,
+  //     NO_PROXY   : 'GOOGLE.COM'
+  //   }
+  // }, false)
 
-  runTest('NO_PROXY ignored with explicit proxy passed', {
-    env   : { NO_PROXY : '*' },
-    proxy : s.url
-  }, true)
+  // runTest('NO_PROXY ignored with explicit proxy passed', {
+  //   env   : { NO_PROXY : '*' },
+  //   proxy : s.url
+  // }, true)
 
-  runTest('NO_PROXY overrides HTTP_PROXY for specific hostname', {
-    env : {
-      HTTP_PROXY : s.url,
-      NO_PROXY   : 'google.com'
-    }
-  }, false)
+  // runTest('NO_PROXY overrides HTTP_PROXY for specific hostname', {
+  //   env : {
+  //     HTTP_PROXY : s.url,
+  //     NO_PROXY   : 'google.com'
+  //   }
+  // }, false)
 
-  runTest('no_proxy overrides HTTP_PROXY for specific hostname', {
-    env : {
-      HTTP_PROXY : s.url,
-      no_proxy   : 'google.com'
-    }
-  }, false)
+  // runTest('no_proxy overrides HTTP_PROXY for specific hostname', {
+  //   env : {
+  //     HTTP_PROXY : s.url,
+  //     no_proxy   : 'google.com'
+  //   }
+  // }, false)
 
-  runTest('NO_PROXY does not override HTTP_PROXY if no hostnames match', {
-    env : {
-      HTTP_PROXY : s.url,
-      NO_PROXY   : 'foo.bar,bar.foo'
-    }
-  }, true)
+  // runTest('NO_PROXY does not override HTTP_PROXY if no hostnames match', {
+  //   env : {
+  //     HTTP_PROXY : s.url,
+  //     NO_PROXY   : 'foo.bar,bar.foo'
+  //   }
+  // }, true)
 
-  runTest('NO_PROXY overrides HTTP_PROXY if a hostname matches', {
-    env : {
-      HTTP_PROXY : s.url,
-      NO_PROXY   : 'foo.bar,google.com'
-    }
-  }, false)
+  // runTest('NO_PROXY overrides HTTP_PROXY if a hostname matches', {
+  //   env : {
+  //     HTTP_PROXY : s.url,
+  //     NO_PROXY   : 'foo.bar,google.com'
+  //   }
+  // }, false)
 
-  runTest('NO_PROXY allows an explicit port', {
-    env : {
-      HTTP_PROXY : s.url,
-      NO_PROXY   : 'google.com:80'
-    }
-  }, false)
+  // runTest('NO_PROXY allows an explicit port', {
+  //   env : {
+  //     HTTP_PROXY : s.url,
+  //     NO_PROXY   : 'google.com:80'
+  //   }
+  // }, false)
 
-  runTest('NO_PROXY only overrides HTTP_PROXY if the port matches', {
-    env : {
-      HTTP_PROXY : s.url,
-      NO_PROXY   : 'google.com:1234'
-    }
-  }, true)
+  // runTest('NO_PROXY only overrides HTTP_PROXY if the port matches', {
+  //   env : {
+  //     HTTP_PROXY : s.url,
+  //     NO_PROXY   : 'google.com:1234'
+  //   }
+  // }, true)
 
-  runTest('NO_PROXY=* should override HTTP_PROXY for all hosts', {
-    env : {
-      HTTP_PROXY : s.url,
-      NO_PROXY   : '*'
-    }
-  }, false)
+  // runTest('NO_PROXY=* should override HTTP_PROXY for all hosts', {
+  //   env : {
+  //     HTTP_PROXY : s.url,
+  //     NO_PROXY   : '*'
+  //   }
+  // }, false)
 
-  runTest('NO_PROXY should override HTTP_PROXY for all subdomains', {
-    env : {
-      HTTP_PROXY : s.url,
-      NO_PROXY   : 'google.com'
-    },
-    headers : { host : 'www.google.com' }
-  }, false)
+  // runTest('NO_PROXY should override HTTP_PROXY for all subdomains', {
+  //   env : {
+  //     HTTP_PROXY : s.url,
+  //     NO_PROXY   : 'google.com'
+  //   },
+  //   headers : { host : 'www.google.com' }
+  // }, false)
 
-  runTest('NO_PROXY should not override HTTP_PROXY for partial domain matches', {
-    env : {
-      HTTP_PROXY : s.url,
-      NO_PROXY   : 'oogle.com'
-    }
-  }, true)
+  // runTest('NO_PROXY should not override HTTP_PROXY for partial domain matches', {
+  //   env : {
+  //     HTTP_PROXY : s.url,
+  //     NO_PROXY   : 'oogle.com'
+  //   }
+  // }, true)
 
-  runTest('NO_PROXY with port should not override HTTP_PROXY for partial domain matches', {
-    env : {
-      HTTP_PROXY : s.url,
-      NO_PROXY   : 'oogle.com:80'
-    }
-  }, true)
+  // runTest('NO_PROXY with port should not override HTTP_PROXY for partial domain matches', {
+  //   env : {
+  //     HTTP_PROXY : s.url,
+  //     NO_PROXY   : 'oogle.com:80'
+  //   }
+  // }, true)
 
-  runTest('proxy: null should override HTTP_PROXY', {
-    env     : { HTTP_PROXY : s.url },
-    proxy   : null,
-    timeout : 500
-  }, false)
+  // runTest('proxy: null should override HTTP_PROXY', {
+  //   env     : { HTTP_PROXY : s.url },
+  //   proxy   : null,
+  //   timeout : 500
+  // }, false)
 
-  runTest('uri auth without proxy auth', {
-    url   : 'http://user:pass@google.com',
-    proxy : s.url
-  }, function(t, req, res) {
-    t.equal(req.headers['proxy-authorization'], undefined)
-    t.equal(req.headers.authorization, 'Basic dXNlcjpwYXNz')
-  })
+  // runTest('uri auth without proxy auth', {
+  //   url   : 'http://user:pass@google.com',
+  //   proxy : s.url
+  // }, function(t, req, res) {
+  //   t.equal(req.headers['proxy-authorization'], undefined)
+  //   t.equal(req.headers.authorization, 'Basic dXNlcjpwYXNz')
+  // })
 
   runTest('proxy https over http', {
     url   : 'https://google.com',
